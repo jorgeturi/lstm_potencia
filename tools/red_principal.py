d2d5c046611d75a594197b5ab8eee466c558dd3e
jorge <jorgeturina@gmail.com>
2024-12-13 11:09:06 -0300

    otro model

--- tools/red_principal.py
+++ tools/red_principal.py
@@ -756,7 +756,7 @@ def entrenar_modelo(Xtrain, ytrain, Xval, yval, path_guardado='modelo_entrenado.
     # Crear el modelo LSTM
     model = Sequential()
 
-    model.add(LSTM(512, return_sequences=False, input_shape=(Xtrain.shape[1], Xtrain.shape[2]), kernel_initializer=initializer ) )
+    model.add(LSTM(256, return_sequences=False, input_shape=(Xtrain.shape[1], Xtrain.shape[2]), kernel_initializer=initializer ) )
     #model.add(Dropout(0.1))
     model.add(BatchNormalization())
 
@@ -783,7 +783,7 @@ def entrenar_modelo(Xtrain, ytrain, Xval, yval, path_guardado='modelo_entrenado.
 
     try:
         # Entrenar el modelo con datos de validación, EarlyStopping y ModelCheckpoint
-        model.fit(Xtrain, ytrain, epochs=300, verbose=1, batch_size=32,
+        model.fit(Xtrain, ytrain, epochs=300, verbose=1, batch_size=128,
                   validation_data=(Xval, yval), callbacks=[early_stopping, checkpoint])
     except MemoryError as e:
         print("Error de memoria: ", e)
