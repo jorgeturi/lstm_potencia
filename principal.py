20acc773739ad1d6b987b3a6c63ef16eb3871d70
jorge <jorgeturina@gmail.com>
2024-12-18 16:08:01 -0300

    modelo

--- principal.py
+++ principal.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     # nombre del modelo, con esto se crea la carpeta y archivos salida
     ## salida.entrada.bz.variacion
-    nombre_modelo = "modelo 0.0.4.0" 
+    nombre_modelo = "modelo 0.0.5.0" 
     carpeta, resultados_path = crear_carpeta_y_guardar(nombre_modelo)
 
     # Procesamiento de los datos
--- tools/red_principal.py
+++ tools/red_principal.py
@@ -802,7 +802,7 @@ def entrenar_modelo(Xtrain, ytrain, Xval, yval, path_guardado='modelo_entrenado.
 
     try:
         # Entrenar el modelo con datos de validación, EarlyStopping y ModelCheckpoint
-        model.fit(Xtrain, ytrain, epochs=300, verbose=1, batch_size=64,
+        model.fit(Xtrain, ytrain, epochs=300, verbose=1, batch_size=128,
                   validation_data=(Xval, yval), callbacks=[early_stopping, checkpoint])
     except MemoryError as e:
         print("Error de memoria: ", e)
